f54556c4ab4d96cba5a688af7d6657e5d4af9042
skjelinottosen <32384597+skjelinottosen@users.noreply.github.com>
2020-07-04 15:23:51 +0200

    Add files via upload

---
+++ Hashing/ChainedHashClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace Hashing
+{
+    //No rehashing at full table / long lists
+    //Offers insertion and search only
+    public class ChainedHashClass
+    {
+        private class Node
+        {
+            public string Data;
+            public Node Next { get; set; }
+
+            public Node(String data, Node next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+
+        public int HashLength { get; set; }
+        Node[] HashTable;
+        public int NumElements { get; set; }
+        public int NumCollisions { get; set; }
+
+
+        public ChainedHashClass(int hashLength)
+        {
+            HashLength = hashLength;
+            HashTable = new Node[HashLength];
+            NumElements = 0;
+            NumCollisions = 0;
+
+        }
+
+        public float LoadFactor()
+        {
+            return HashLength / NumElements;
+        }
+
+        public int Hash(string s)
+        {
+            int hashValue = Math.Abs(s.GetHashCode());
+            return hashValue % HashLength;
+        }
+
+        public void Insert(string s)
+        {
+            int index = Hash(s);
+            NumElements++;
+
+            if (HashTable[index] != null)
+            {
+                NumCollisions++;
+            }
+
+            HashTable[index] = new Node(s, HashTable[index]);
+        }
+
+        public bool Search(string s)
+        {
+            Node node = HashTable[Hash(s)];
+
+            while (node != null)
+            {
+                if (node.Data == s)
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Hashing/LinearHashClass.cs
@@ -0,0 +1,93 @@
+﻿using System;
+namespace Hashing
+{
+    //No rehashing at full table / long lists
+    //Offers insertion and search only
+    public class LinearHashClass
+    {
+        public int HashLength { get; set; }
+        private String[] HashTable;
+        public int NumElements { get; set; }
+        public int NumProbes { get; set; }
+
+        public LinearHashClass(int hashLength)
+        {
+            HashLength = hashLength;
+            HashTable = new String[HashLength];
+            NumElements = 0;
+            NumProbes = 0;
+        }
+
+        public float Loadfactor()
+        {
+            return (float)NumElements / HashLength;
+        }
+
+
+        public int Hash(string s)
+        {
+            int hashValue = Math.Abs(s.GetHashCode());
+            return hashValue % HashLength;
+        }
+
+        //Insert with quadratic probing
+        public void Insert(string s)
+        {
+            int index = Hash(s);
+            int step = 0;
+            int next = index;
+
+            while (HashTable[next] != null)
+            {
+                step++;
+                NumProbes++;
+                next = index + (step * step);
+
+                //Wrap around
+                if (next >= HashLength)
+                {
+                    next %= HashLength;
+                }
+
+                if (next == HashLength)
+                {
+                    Console.WriteLine("Error: Hashtable is full");
+                }
+            }
+
+            //Stores value in hashtable at index "next"
+            HashTable[next] = s;
+            NumElements++;
+        }
+
+        public bool Search(string s)
+        {
+            int index = Hash(s);
+            int step = 0;
+            int next = index;
+
+            while (HashTable[next] != null)
+            {
+                if (HashTable[next].CompareTo(s) == 0)
+                {
+                    return true;
+                }
+
+                step++;
+                next = index + (step * step);
+
+                //Wrap around
+                if (next >= HashLength)
+                {
+                    next %= HashLength;
+                }
+
+                if (next == HashLength)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
