b3031dca55c081c2e0177465a213f6092c88af8a
skjelinottosen <32384597+skjelinottosen@users.noreply.github.com>
2020-07-14 19:19:45 +0200

    Add files via upload

---
+++ Sorting/SequentialSorting/BubbleSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace SequentialSorting
+{
+    //Slow because of all the swaping O(n2)(not often used).
+    public class BubbleSort
+    {
+        public BubbleSort()
+        {
+        }
+
+        public void Sort(int[] array)
+        {
+            int length = array.Length;
+            int tmp;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
